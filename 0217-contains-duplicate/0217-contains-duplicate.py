a86eb967e89840dfc9495862184e5ac930b33830
thiyura thilakshana <157218559+2tzz@users.noreply.github.com>
2025-09-06 08:36:16 +0530

    Time: 97 ms (5.11%), Space: 38.2 MB (9.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,19 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
         my_dict = {}
         for i in range(0,len(nums)) :
             my_dict[i] = nums[i]
-        print(my_dict)
 
-        reverse_multidict = {}
-        for key, value in my_dict.items():
-            reverse_multidict.setdefault(value, []).append(key)
-
-        duplicate_values_found = {}
-        for value, keys in reverse_multidict.items():
-            if len(keys) > 1:
-                duplicate_values_found[value] = keys
-        if len(duplicate_values_found) == 0 :
-            return False
-        elif len(duplicate_values_found) > 0 :
-            return True
+        for i in range(0,len(my_dict)-1):
+            if my_dict[i]  == my_dict[i+1] :
+                return True      
+        return False
