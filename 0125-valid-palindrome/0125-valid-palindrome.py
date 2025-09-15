7c68d0d97884207113bf362c1528851fc0618789
thiyura thilakshana <157218559+2tzz@users.noreply.github.com>
2025-09-15 08:15:01 +0530

    Time: 11 ms (34.77%), Space: 23.8 MB (5.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,22 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if len(s) == 1 :
-            return True
-        new_s = ''
-        s = s.replace(" ", "")
-        for char in s :
-            if char.isalpha() or char.isdigit():
-                new_s += char
-        new_s = list(new_s.lower())
-        print(new_s)
-        s_len = int(len(new_s))
-        print(s_len)
-        new_rev = list(new_s[::-1])
-        print(new_rev)
-        for i in range(0,s_len):
-            if new_s[i] == new_rev[i]:
-                output = True
-            else :
-                return False
-        return True
+        new_s = []
+        for c in s:
+            if c.isalpha() or c.isdigit():
+                new_s.append(c.lower())
+        return new_s == new_s[::-1]
         
