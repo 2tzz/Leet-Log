d611cd1ed6740e1475829a91652a359d9d554dfd
thiyura thilakshana <157218559+2tzz@users.noreply.github.com>
2025-09-15 08:56:21 +0530

    Time: 0 ms (100%), Space: 18.6 MB (33.18%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        lookup = {}
+        output = []
+        for i, num in enumerate(numbers):
+            diff = target - num
+            if diff in lookup:
+                output = [lookup[diff] + 1, i + 1]  # 1-based index
+                break
+            lookup[num] = i
+
+        return output
+        
